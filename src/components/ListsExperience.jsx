1498b9ac03049e40997c75bce303ae988540a99e
Rafansa Oktaviano <ravansaoktaviano@gmail.com>
2025-06-24 12:06:34 +0700

    fix responsive on mobile

--- src/components/ListsExperience.jsx
+++ src/components/ListsExperience.jsx
@@ -4,7 +4,7 @@ import logosenja from "../assets/logo-senja.png";
 const ListsExperience = () => {
   return (
     <div className="w-full lg:w-[50%]">
-      <div className="w-full bg-slate-200 h-[100px] rounded-xl flex items-center justify-between gap-4  p-4 border mb-4">
+      <div className="w-full bg-slate-200 h-auto flex-wrap  sm:flex-nowrap justify-between sm:h-[100px] rounded-xl flex items-center  gap-4  p-4 border mb-4">
         <div className="flex gap-4 ">
           <img
             src={logosenja}
@@ -21,16 +21,18 @@ const ListsExperience = () => {
             </p>
           </div>
         </div>
-        <a
-          href="https://dashboard.rebill-pos.com/demo"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ml-[40px] sm:ml-0 bg-blue-500 hover:bg-blue-600 text-white text-sm px-4 py-2 rounded-lg transition"
-        >
-          Demo
-        </a>
+        <div className="w-full sm:w-auto flex justify-end sm:justify-start">
+          <a
+            href="https://dashboard.rebill-pos.com/demo"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="ml-[40px] sm:ml-0 bg-blue-500 hover:bg-blue-600 text-white text-sm px-4 py-2 rounded-lg transition"
+          >
+            Demo
+          </a>
+        </div>
       </div>
-      <div className="w-full bg-slate-200 h-[100px] rounded-xl flex items-center justify-between gap-4  p-4 border ">
+      <div className="w-full bg-slate-200 h-auto flex flex-wrap items-center gap-4 p-4 border rounded-xl sm:flex-nowrap sm:justify-between sm:h-[100px] justify-between">
         <div className="flex gap-4 ">
           <img
             src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" // briefcase
@@ -39,7 +41,7 @@ const ListsExperience = () => {
           />
 
           <div className="flex flex-col">
-            <h3 className="  text-slate-700 font-semibold tracking-tight text-xl">
+            <h3 className="  text-slate-700 whitespace-nowrap font-semibold tracking-tight text-xl">
               Frontend Developer
             </h3>
             <p className="text-slate-700 font-semibold tracking-tight text-sm">
@@ -47,14 +49,16 @@ const ListsExperience = () => {
             </p>
           </div>
         </div>
-        <a
-          href="https://hummerdream.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className=" ml-[40px] sm:ml-0  bg-blue-500 hover:bg-blue-600 text-white text-sm px-4 py-2 rounded-lg transition"
-        >
-          Demo
-        </a>
+        <div className="w-full sm:w-auto flex justify-end sm:justify-start">
+          <a
+            href="https://hummerdream.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-blue-500 hover:bg-blue-600 text-white text-sm px-4 py-2 rounded-lg transition"
+          >
+            Demo
+          </a>
+        </div>
       </div>
     </div>
   );
